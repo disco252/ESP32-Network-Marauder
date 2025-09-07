99a7bbc0d90563c6315f766044a386a48efc5d0f
disco252 <printsandturns@gmail.com>
2025-09-07 12:52:22 -0500

    Create EthernetDiagnostics.h

---
+++ esp32_marauder/EthernetDiagnostics.h
@@ -0,0 +1,23 @@
+// File: esp32_marauder/EthernetDiagnostics.h
+#ifndef ETHERNET_DIAGNOSTICS_H
+#define ETHERNET_DIAGNOSTICS_H
+
+#include <Arduino.h>
+#include <ETH.h>
+#include <WiFi.h>
+
+class EthernetDiagnostics {
+private:
+    bool ethConnected;
+    IPAddress localIP;
+    IPAddress gateway;
+    
+public:
+    void init();
+    void scanDHCPServers();
+    void performTraceroute(IPAddress target);
+    void pingTest(IPAddress target, int count);
+    bool isConnected();
+};
+
+#endif
